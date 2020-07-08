08a306ef367f736cd1316e09c6197d8424d988bf
Nikolaev Viktor <maclauddd@gmail.com>
2020-07-08 07:54:44 +0300

    feat (server): added more data in schema query

--- server/src/schema/Query.ts
+++ server/src/schema/Query.ts
@@ -4,5 +4,9 @@ export const Query = queryType({
   definition(t) {
     t.crud.post();
     t.crud.posts({ filtering: true, ordering: true });
+    t.crud.user();
+    t.crud.users({ filtering: true, ordering: true });
+    t.crud.comment();
+    t.crud.comments({ filtering: true, ordering: true });
   },
 });
